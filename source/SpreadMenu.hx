4a2dd720918d2bfa5cfc58643257a147fc66b921
Aladdin Lee <siraj.n.lee@gmail.com>
2022-03-18 10:36:04 -0700

    Added Spread and Settings menus boilerplate

---
+++ source/SpreadMenu.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxState;
+
+class SpreadMenu extends FlxState
+{
+	override function create()
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
